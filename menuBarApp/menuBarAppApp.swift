fae643a0341af32f5aed5c98dbc0c82019200457
hoit1011 <parksm071011@gmail.com>
2025-03-13 23:19:09 +0900

    Initial Commit

---
+++ menuBarApp/menuBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuBarAppApp.swift
+//  menuBarApp
+//
+//  Created by 박성민 on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct menuBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
